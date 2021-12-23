086a127a47ca8de7dfb783f6d2b7a95e020987cf
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-12-22 22:06:43 -0600

    Initial base

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to api-rest
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/api_rest/core.clj
@@ -0,0 +1,14 @@
+(ns api-rest.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
+  (:gen-class))  
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
