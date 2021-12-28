03c1e355a1f3a3babbd7922ca5cdcadaf0649765
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-12-27 23:24:37 -0600

    Latest changes

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject api-rest "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "API-REST CloJure Playground by AEMN"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [compojure "1.6.1"]
-                 [http-kit "2.3.0"]
-                 [ring/ring-defaults "0.3.2"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [ring/ring-core "1.9.4"]
+                 [ring/ring-jetty-adapter "1.9.4"]
+                 [metosin/reitit "0.5.15"]
+                 [metosin/muuntaja "0.6.8"]]
+
   :main ^:skip-aot api-rest.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/api_rest/core.clj
+++ src/api_rest/core.clj
@@ -1,14 +1,42 @@
 (ns api-rest.core
-  (:require [org.httpkit.server :as server]
-            [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer :all]
-            [clojure.pprint :as pp]
-            [clojure.string :as str]
-            [clojure.data.json :as json])
-  (:gen-class))  
+  (:require [ring.adapter.jetty :as ring-jetty]
+            [muuntaja.core :as m]
+            [reitit.ring :as ring]
+            [reitit.ring.middleware.muuntaja :as muuntaja])
+  (:gen-class))
+
+(defn handle-query-one-car [_]
+  {:status 200
+   :body "query-one"})
+
+(defn handle-query-all-cars [_]
+  {:status 200
+   :body "query-all"})
+
+(defn handle-add-car [_]
+  {:status 200
+   :body "add-car"})
+
+(defn handle-edit-car [_]
+  {:status 200
+   :body "edit-car"})
+
+(defn handle-delete-car [_]
+  {:status 200
+   :body "delete-car"})
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    ["/"
+     ["car" {:get handle-query-one-car}]
+     ["cars" {:get handle-query-all-cars}]
+     ["car" {:post handle-add-car}]
+     ["car" {:put handle-edit-car}]
+     ["car" {:delete handle-delete-car}]])))
+
+(ring-jetty/run-jetty app {:port 3000
+                           :join? false})
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& args])
