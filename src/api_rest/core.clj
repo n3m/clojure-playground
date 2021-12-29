63a52c8a4d09ad58fbb20f779a49fac2a177ae6d
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-12-28 21:46:19 -0600

    Get one and Get all finished

--- src/api_rest/core.clj
+++ src/api_rest/core.clj
@@ -10,165 +10,226 @@
 
   (:gen-class))
 
-(def cars {:1 {:id 1
+(def cars {:1  {:id 1
 
-               :name "Ford"
+                :name "Ford"
 
-               :price "100000"
+                :price "100000"
 
-               :year "2000"
+                :year "2000"
+
+                :color "red"
+
+                :type "car"
+
+                :brand "Ford"
+
+                :model "Mustang"
+
+                :engine "V8"}
+
+           :2 {:id 2
+
+               :name "Audi"
+
+               :price "200000"
+
+               :year "2010"
+
+               :color "blue"
+
+               :type "car"
+
+               :brand "Audi"
+
+               :model "A4"
+
+               :engine "V8"}
+
+           :3 {:id 3
+
+               :name "BMW"
+
+               :price "300000"
+
+               :year "2010"
+
+               :color "black"
+
+               :type "car"
+
+               :brand "BMW"
+
+               :model "X5"
+
+               :engine "V8"}
+
+           :4 {:id 4
+
+               :name "Mercedes"
+
+               :price "400000"
+
+               :year "2010"
+
+               :color "white"
+
+               :type "car"
+
+               :brand "Mercedes"
+
+               :model "S-Class"
+
+               :engine "V8"}
+
+           :5 {:id 5
+
+               :name "Toyota"
+
+               :price "500000"
+
+               :year "2010"
+
+               :color "green"
+
+               :type "car"
+
+               :brand "Toyota"
+
+               :model "Corolla"
+
+               :engine "V8"}
+
+           :6 {:id 6
+
+               :name "Honda"
+
+               :price "600000"
+
+               :year "2010"
+
+               :color "yellow"
+
+               :type "car"
+
+               :brand "Honda"
+
+               :model "Civic"
+
+               :engine "V8"}
+
+           :7 {:id 7
+
+               :name "Nissan"
+
+               :price "700000"
+
+               :year "2010"
 
                :color "red"
 
                :type "car"
 
-               :brand "Ford"
+               :brand "Nissan"
 
-               :model "Mustang"
+               :model "Sentra"
 
-               :engine "V8"}, :2 {:id 2
+               :engine "V8"}
 
-                                  :name "Audi"
+           :8 {:id 8
 
-                                  :price "200000"
+               :name "Opel"
 
-                                  :year "2010"
+               :price "800000"
 
-                                  :color "blue"
+               :year "2010"
 
-                                  :type "car"
+               :color "blue"
 
-                                  :brand "Audi"
+               :type "car"
 
-                                  :model "A4"
+               :brand "Opel"
 
-                                  :engine "V8"}, :3 {:id 3
+               :model "Astra"
 
-                                                     :name "BMW"
+               :engine "V8"}})
 
-                                                     :price "300000"
+(defn handle-query-one-car [{requestParams :path-params}]
 
-                                                     :year "2010"
+  (def paramId (get requestParams :id))
 
-                                                     :color "black"
+  (let [car ((keyword paramId) cars nil)]
 
-                                                     :type "car"
+    (if (not (nil? car))
 
-                                                     :brand "BMW"
+      {:status 200
 
-                                                     :model "X5"
+       :body car}
 
-                                                     :engine "V8"}, :4 {:id 4
+      {:status 404
 
-                                                                        :name "Mercedes"
-
-                                                                        :price "400000"
-
-                                                                        :year "2010"
-
-                                                                        :color "white"
-
-                                                                        :type "car"
-
-                                                                        :brand "Mercedes"
-
-                                                                        :model "S-Class"
-
-                                                                        :engine "V8"}, :5 {:id 5
-
-                                                                                           :name "Toyota"
-
-                                                                                           :price "500000"
-
-                                                                                           :year "2010"
-
-                                                                                           :color "green"
-
-                                                                                           :type "car"
-
-                                                                                           :brand "Toyota"
-
-                                                                                           :model "Corolla"
-
-                                                                                           :engine "V8"}, :6 {:id 6
-
-                                                                                                              :name "Honda"
-
-                                                                                                              :price "600000"
-
-                                                                                                              :year "2010"
-
-                                                                                                              :color "yellow"
-
-                                                                                                              :type "car"
-
-                                                                                                              :brand "Honda"
-
-                                                                                                              :model "Civic"
-
-                                                                                                              :engine "V8"}, :7 {:id 7
-
-                                                                                                                                 :name "Nissan"
-
-                                                                                                                                 :price "700000"
-
-                                                                                                                                 :year "2010"
-
-                                                                                                                                 :color "red"
-
-                                                                                                                                 :type "car"
-
-                                                                                                                                 :brand "Nissan"
-
-                                                                                                                                 :model "Sentra"
-
-                                                                                                                                 :engine "V8"}, :8 {:id 8
-
-                                                                                                                                                    :name "Opel"
-
-                                                                                                                                                    :price "800000"
-
-                                                                                                                                                    :year "2010"
-
-                                                                                                                                                    :color "blue"
-
-                                                                                                                                                    :type "car"
-
-                                                                                                                                                    :brand "Opel"
-
-                                                                                                                                                    :model "Astra"
-
-                                                                                                                                                    :engine "V8"}})
-
-(defn handle-query-one-car [_]
-
-  {:status 200
-
-   :body "query-one"})
+       :body {:error "Car not found"}})))
 
 (defn handle-query-all-cars [_]
 
   {:status 200
 
-   :body "query-all"})
+   :body cars})
 
-(defn handle-add-car [_]
+(defn handle-add-car [{car :body-params}]
 
-  {:status 200
+  (let [id (str (java.util.UUID/randomUUID))] ;; Creates a new UUID
 
-   :body "add-car"})
+
+    {:status 200
+
+     :body "id"}))
+
 
 (defn handle-edit-car [_]
 
+
+
+
+
+
+
   {:status 200
+
+
+
+
+
+
 
    :body "edit-car"})
 
+
+
+
+
+
+
 (defn handle-delete-car [_]
+
+
+
+
+
+
 
   {:status 200
 
+
+
+
+
+
+
    :body "delete-car"})
+
+
+
 
 (def app
 
@@ -178,15 +239,19 @@
 
     ["/"
 
-     ["car" {:get handle-query-one-car}]
+     ["car/:id" {:get handle-query-one-car}]
 
      ["cars" {:get handle-query-all-cars}]
 
      ["car" {:post handle-add-car}]
-
      ["car" {:put handle-edit-car}]
+     ["car" {:delete handle-delete-car}]
+     
+     ]
 
-     ["car" {:delete handle-delete-car}]])))
+    {:data {:muuntaja m/instance
+
+            :middleware [muuntaja/format-middleware]}})))
 
 (ring-jetty/run-jetty app {:port 3000
 
