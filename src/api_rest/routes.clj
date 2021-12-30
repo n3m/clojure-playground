44ec712209019659e14626f6b2962e5447d8388d
Alan Enrique Maldonado Navarro <business.alanm@gmail.com>
2021-12-30 03:25:50 -0600

    Latest changes

--- src/api_rest/routes.clj
+++ src/api_rest/routes.clj
@@ -46,114 +46,7 @@
                :model "A4"
 
                :engine "V8"}
-
-           :2 {:id 2
-
-               :name "BMW"
-
-               :price "300000"
-
-               :year "2010"
-
-               :color "black"
-
-               :type "car"
-
-               :brand "BMW"
-
-               :model "X5"
-
-               :engine "V8"}
-
-           :3 {:id 3
-
-               :name "Mercedes"
-
-               :price "400000"
-
-               :year "2010"
-
-               :color "white"
-
-               :type "car"
-
-               :brand "Mercedes"
-
-               :model "S-Class"
-
-               :engine "V8"}
-
-           :4 {:id 4
-
-               :name "Toyota"
-
-               :price "500000"
-
-               :year "2010"
-
-               :color "green"
-
-               :type "car"
-
-               :brand "Toyota"
-
-               :model "Corolla"
-
-               :engine "V8"}
-
-           :5 {:id 5
-
-               :name "Honda"
-
-               :price "600000"
-
-               :year "2010"
-
-               :color "yellow"
-
-               :type "car"
-
-               :brand "Honda"
-
-               :model "Civic"
-
-               :engine "V8"}
-
-           :6 {:id 6
-
-               :name "Nissan"
-
-               :price "700000"
-
-               :year "2010"
-
-               :color "red"
-
-               :type "car"
-
-               :brand "Nissan"
-
-               :model "Sentra"
-
-               :engine "V8"}
-
-           :7 {:id 7
-
-               :name "Opel"
-
-               :price "800000"
-
-               :year "2010"
-
-               :color "blue"
-
-               :type "car"
-
-               :brand "Opel"
-
-               :model "Astra"
-
-               :engine "V8"}}))
+           }))
 
 (defn handle-query-one-car [id]
 
@@ -181,13 +74,11 @@
 
     (let [document (assoc jsonBody "id" id)]
       
-      (pprint/pprint document)
-      
       (dosync (swap! cars assoc (keyword (str id)) document))
     
       {:status 200
 
-       :body @cars})
+       :body document})
     )
 
   )
